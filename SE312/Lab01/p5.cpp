b08c5b1e3cfd556a0a7c2050769722366da763f1
knah1d <128985563+knah1d@users.noreply.github.com>
2023-08-01 15:49:37 +0600

    Update p5.cpp

--- SE312/Lab01/p5.cpp
+++ SE312/Lab01/p5.cpp
@@ -1,29 +1,29 @@
-Sure! To implement a DFA (Deterministic Finite Automaton) transition function in C, you can use a switch statement or an array-based approach.
-
-Here's an example using a switch statement:
+Sure! Below is a C program that implements a DFA to recognize the language L over the alphabet Σ(0, 1), where the language L consists of strings ending with "00".
 
 ```c
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 // Define the DFA transition function
 int dfa_transition(int state, char input) {
     switch (state) {
         case 0:
-            if (input == 'a')
+            if (input == '0')
                 return 1;
-            else if (input == 'b')
+            else if (input == '1')
                 return 0;
             break;
         case 1:
-            if (input == 'a')
-                return 1;
-            else if (input == 'b')
+            if (input == '0')
                 return 2;
+            else if (input == '1')
+                return 0;
             break;
         case 2:
-            if (input == 'a')
-                return 1;
-            else if (input == 'b')
+            if (input == '0')
+                return 2;
+            else if (input == '1')
                 return 0;
             break;
     }
@@ -31,28 +31,40 @@ int dfa_transition(int state, char input) {
     return -1;
 }
 
-int main() {
-    char input_string[] = "abaa"; // Input string to process
+// Function to check if the input string is accepted by the DFA
+bool is_string_accepted(char input_string[]) {
     int current_state = 0; // Start from the initial state (e.g., 0)
 
-    // Process the input string character by character
+    printf("Path of Transition: ");
     for (int i = 0; input_string[i] != '\0'; i++) {
+        printf("%d -> ", current_state);
         current_state = dfa_transition(current_state, input_string[i]);
         if (current_state == -1) {
             printf("Invalid transition at character %c\n", input_string[i]);
-            break;
+            return false;
         }
     }
+    printf("%d\n", current_state);
 
-    // Check if the final state is an accepting state (e.g., 1, 2, etc.)
-    if (current_state == 1 || current_state == 2) {
-        printf("String accepted!\n");
+    // Check if the final state is an accepting state (e.g., 2).
+    return current_state == 2;
+}
+
+int main() {
+    char input_string[100]; // Input string to process
+
+    printf("Enter the input string: ");
+    scanf("%s", input_string);
+
+    // Check if the input string is accepted by the DFA
+    if (is_string_accepted(input_string)) {
+        printf("Accepted\n");
     } else {
-        printf("String not accepted!\n");
+        printf("Rejected\n");
     }
 
     return 0;
 }
 ```
 
-In this example, the DFA has three states (0, 1, and 2) and accepts strings with an even number of 'b's. You can modify the `dfa_transition` function and the main program to match the transition rules and accepting states for your specific DFA.
+In this example, the DFA has three states (0, 1, and 2), with state 2 being the accepting state. The DFA accepts strings that end with "00". The input string is taken as user input, and the program will output whether the string is accepted or rejected, along with the path of transitions during processing.
