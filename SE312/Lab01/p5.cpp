a131c31279cdad3138c9efaf24b8706a2ad038b8
knah1d <128985563+knah1d@users.noreply.github.com>
2023-08-01 15:30:40 +0600

    Update p5.cpp

--- SE312/Lab01/p5.cpp
+++ SE312/Lab01/p5.cpp
@@ -1,46 +1,58 @@
-#include<bits/stdc++.h>
-using namespace std;
+Sure! To implement a DFA (Deterministic Finite Automaton) transition function in C, you can use a switch statement or an array-based approach.
+
+Here's an example using a switch statement:
+
+```c
+#include <stdio.h>
+
+// Define the DFA transition function
+int dfa_transition(int state, char input) {
+    switch (state) {
+        case 0:
+            if (input == 'a')
+                return 1;
+            else if (input == 'b')
+                return 0;
+            break;
+        case 1:
+            if (input == 'a')
+                return 1;
+            else if (input == 'b')
+                return 2;
+            break;
+        case 2:
+            if (input == 'a')
+                return 1;
+            else if (input == 'b')
+                return 0;
+            break;
+    }
+    // If no valid transition is found, return an error state (e.g., -1).
+    return -1;
+}
 
 int main() {
+    char input_string[] = "abaa"; // Input string to process
+    int current_state = 0; // Start from the initial state (e.g., 0)
 
-    int i, j, k;
-    char w[50], x[50], y[50];
-    cin>>w;
-
-    for(i = 0; w[i]!='\0'; i++); //length
-    if(i%2==0) {
-        for(j = 0; j<i/2; j++) {
-            x[j] = w[j];
-        }
-        x[j] = '\0';
-
-        for(j = i/2, k = 0; w[j]!='\0'; j++, k++) {
-            y[k] = w[j];
-        }
-        y[k] = '\0';
-
-        //cout<<x<<endl<<y;
-    }
-    else {
-        for(j = 0; j<i/2; j++) {
-            x[j] = w[j];
-        }
-        x[j] = '\0';
-
-        for(j = (i/2)+1, k = 0; w[j]!='\0'; j++, k++) {
-            y[k] = w[j];
-        }
-        y[k] = '\0';
-        //cout<<x<<endl<<y<<endl;
-    }
-    
-    
-    for(j = 0, k = (i/2)-1; x[j]!='\0'; j++, k--) {
-        if(x[j]!=y[k]) {
-            cout<<"NOT palindrome...:("<<endl;
-            return 0;
+    // Process the input string character by character
+    for (int i = 0; input_string[i] != '\0'; i++) {
+        current_state = dfa_transition(current_state, input_string[i]);
+        if (current_state == -1) {
+            printf("Invalid transition at character %c\n", input_string[i]);
+            break;
         }
     }
-    cout<<"PALINDROME...:)"<<endl;
+
+    // Check if the final state is an accepting state (e.g., 1, 2, etc.)
+    if (current_state == 1 || current_state == 2) {
+        printf("String accepted!\n");
+    } else {
+        printf("String not accepted!\n");
+    }
+
     return 0;
 }
+```
+
+In this example, the DFA has three states (0, 1, and 2) and accepts strings with an even number of 'b's. You can modify the `dfa_transition` function and the main program to match the transition rules and accepting states for your specific DFA.
