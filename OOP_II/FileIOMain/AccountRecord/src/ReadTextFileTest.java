f737432dd9e16f3c87030f97c74205f688f705c0
k.nahid <bsse1437@iit.du.ac.bd>
2023-09-17 12:45:56 +0600

    OOP

---
+++ OOP_II/FileIOMain/AccountRecord/src/ReadTextFileTest.java
@@ -0,0 +1,12 @@
+// ReadTextFileTest.java
+// This program tests class ReadTextFile.
+
+public class ReadTextFileTest {
+    public static void main(String args[]) {
+        ReadTextFile application = new ReadTextFile();
+
+        application.openFile();
+        application.readRecords();
+        application.closeFile();
+    } // end main
+} // end class ReadTextFileTest
---
+++ OOP_II/FileIOMain/FileIO/src/FileDemonstrationTest.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class FileDemonstrationTest {
+    public static void main( String args[] ) {
+        Scanner input = new Scanner( System.in );
+        FileDemonstration application = new FileDemonstration();
+        System.out.print( "Enter file or directory name here: " );
+        application.analyzePath( input.nextLine() );
+    }
+}
